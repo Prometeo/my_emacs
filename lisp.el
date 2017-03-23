8f3f84f8dac22a68ae9cb717305490588bcc8297
Xairi <xairi.valdivia@wunderman.com>
2017-03-22 19:06:16 -0500

    Configured lisp-mode

--- lisp.el
+++ lisp.el
@@ -1,2 +1,13 @@
 (require 'lisp-mode)
 (add-to-list 'auto-mode-alist '("\\.el$" . lisp-mode))
+
+(defun my-lisp-hooks()
+    (progn
+     (eval-after-load "company"
+              '(progn
+                   (unless (member 'company-elisp (car company-backends))
+                            (setq comp-back (car company-backends))
+                            (push 'company-elisp comp-back)
+                            (setq company-backends (list comp-back)))
+                   ))))
+(add-hook 'lisp-interaction-mode-hook 'my-lisp-hooks)
